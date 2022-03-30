17b1ea806013f5a101968ecfed91dd8bd95ff575
sjkchang <sjkchang@gmail.com>
2022-03-30 13:44:52 -0700

    Edited port

--- src/server.js
+++ src/server.js
@@ -15,7 +15,9 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Example app listening on port 3000!")
+);
 
 module.exports = {
   app,
